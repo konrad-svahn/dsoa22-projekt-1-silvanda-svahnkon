a69bdae1bf8efc133e3893bb7ab05d8e044dbcc9
silvanda@arcada.fi <silvanda@arcada.fi>
2022-02-14 19:27:26 +0200

    uppg 3

--- dsoa22-projekt-1-silvanda-svahnkon/src/main/java/Node.java
+++ dsoa22-projekt-1-silvanda-svahnkon/src/main/java/Node.java
@@ -7,6 +7,7 @@ public class Node {
     private double latitude; 
     private double longitude;
     private ArrayList<Node> neighbours;
+    private Node previous; 
     
     public Node(String name, double latitude, double longitude) {
         this.name = name;
@@ -43,8 +44,38 @@ public class Node {
     public double getLatitude() {
         return latitude;
     }
-
+    
     public double getLongitude() {
         return longitude;
     }
+
+    public double getF(double G, double H){     // Lägger ihop G+H för att få veta den kortaste rutten
+        return G+H;
+    }
+
+    public double calculateH(Node endNode){     //räknar Heuristic distance, den raka längden från noden till mål noden 
+       double heuristicDistance = Utils.getDistance(this.latitude,this.longitude,endNode.getLatitude(),endNode.getLongitude() );
+        return heuristicDistance;
+    }
+    public double calculateG(Node startNode){    // räknar Längen till startnoden
+        double G = 0;
+        //Node current = this.Node; 
+        
+        
+        return G;
+/*
+  · Skapa en Node-variabel current och sätt till innevarande objekt av Node (this)   
+
+    Loopa (så länge som current inte är samma som startNode) {
+        · Beräkna avståndet från current till föregående nod (current.previous).
+        · Addera avståndet till G.
+        · Sätt current att vara samma som current.previous.
+    }
+    returnera G
+    beräkna och returnera G-värdet dvs. i detta fall den verkliga distansen från startnoden startNode till
+    innevarande nod via alla eventuella mellannoder. 
+    Definiera i detta skede även en instansmetod getF som returnerar F-värdet, dvs. G + H.
+    Observera att denna metod inte kan göra sitt jobb på riktigt innan den används i själva algoritmen och noderna får riktiga värden för previous.*/
+    }
+
 }
